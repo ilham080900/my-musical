5580d046c64995d06ef16f8d0428af167a41d12d
ilham080900 <muhammad.ilham080900@gmail.com>
2024-06-18 17:37:26 +0700

    redeploy vercel

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   useListSong();
   useRecomendationPlaylist()
   
-
+  console.log("redeploy")
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header className="mb-2">
