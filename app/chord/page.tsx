259ee4c99ede1554880e0b6c49ff26579e201d6d
ilham080900 <muhammad.ilham080900@gmail.com>
2024-06-21 11:58:06 +0700

    update layout

--- app/chord/page.tsx
+++ app/chord/page.tsx
@@ -21,11 +21,9 @@ const Chord = () => {
         <div className="mb-2 flex flex-col gap-y-6"></div>
       </Header>
       <div className="grid grid-cols-2 xl:grid-cols-1 gap-x-4">
-        <div className="relative flex flex-col md:flex gap-y-2 gap-x-4 p-8">
-          <div className="lg:w-[1325px] md:w-[830px] sm:w-[600px]">
-            <PlayMenu />
-          </div>
-          <div className="relative flex flex-col pt-2 pb-2 bg-neutral-800 lg:w-[1325px] md:w-[830px] sm:w-[600px] h-[auto] rounded-md">
+        <div className="relative flex flex-col md:flex gap-y-2 gap-x-4 p-8 lg:w-[1390px] md:w-[665px] sm:w-[630px]">
+        <PlayMenu />
+          <div className="relative flex flex-col pt-2 pb-2 bg-neutral-800 lg:w-[1325px] md:w-[600px] sm:w-[565px] rounded-md">
             <div
               dangerouslySetInnerHTML={{ __html: htmlContent }}
               className="ml-3 mr-5 mt-2 mb-3"
