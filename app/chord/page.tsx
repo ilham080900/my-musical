77767fdcf60cfd0eca92a68a65f1caec05243f1c
ilham080900 <muhammad.ilham080900@gmail.com>
2024-06-21 13:29:49 +0700

    update terbaru

--- app/chord/page.tsx
+++ app/chord/page.tsx
@@ -21,8 +21,10 @@ const Chord = () => {
         <div className="mb-2 flex flex-col gap-y-6"></div>
       </Header>
       <div className="grid grid-cols-2 xl:grid-cols-1 gap-x-4">
-        <div className="relative flex flex-col md:flex gap-y-2 gap-x-4 p-8 lg:w-[1390px] md:w-[665px] sm:w-[630px]">
-        <PlayMenu />
+        <div className="relative flex flex-col md:flex gap-y-2 gap-x-4 p-8">
+        <div className="lg:w-[1325px] md:w-[600px] sm:w-[565px]">
+            <PlayMenu />
+          </div>
           <div className="relative flex flex-col pt-2 pb-2 bg-neutral-800 lg:w-[1325px] md:w-[600px] sm:w-[565px] rounded-md">
             <div
               dangerouslySetInnerHTML={{ __html: htmlContent }}
